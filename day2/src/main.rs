944957739e1de4058e35e8892e59d221a8b8fdbe
ChrisRG <chris.geekie@gmail.com>
2020-12-02 23:56:34 +0100

    Refactored stuff

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -20,38 +20,39 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = parse_file("input.txt");
-    let passwords: Vec<Password> = parse_lines(input); 
     let mut old_count = 0;
     let mut new_count = 0;
 
-    for password in passwords {
-       if old_validate_password(&password) {
-           old_count += 1;
-       }
-       if new_validate_password(&password) {
-           new_count += 1;
-       }
+    for line in input {
+        if line != "" {
+            if old_validate_password(parse_line(&line)) {
+                old_count += 1;
+            }
+            if new_validate_password(parse_line(&line)) {
+                new_count +=1 ;
+            }
+        }
     }
+
     println!("Part 1: Final count of valid passwords in old system: {}", old_count);
     println!("Part 2: Final count of valid passwords in new system: {}", new_count); 
     Ok(())
 }
 
 struct Password {
-    range: Vec<usize>,
+    lower: usize,
+    upper: usize,
     letter: char,
     password: String,
 }
 
 // Part 1
-fn old_validate_password(pass: &Password) -> bool {
-    let min = pass.range[0];
-    let max = pass.range[1];
-    let password = &pass.password;
+fn old_validate_password(pass: Password) -> bool {
+    let password = pass.password;
     let number_matches = password.matches(pass.letter)
         .collect::<Vec<&str>>().len();
 
-    if number_matches >= min && number_matches <= max {
+    if number_matches >= pass.lower && number_matches <= pass.upper {
         true
     } else {
         false
@@ -59,9 +60,9 @@ fn old_validate_password(pass: &Password) -> bool {
 }
 
 // Part 2
-fn new_validate_password(pass: &Password) -> bool {
-    let pos1 = pass.range[0] - 1;
-    let pos2 = pass.range[1] - 1;
+fn new_validate_password(pass: Password) -> bool {
+    let pos1 = pass.lower - 1;
+    let pos2 = pass.upper - 1;
     let letter = pass.letter;
     let chars: Vec<char> = pass.password.chars().collect();
 
@@ -73,29 +74,21 @@ fn new_validate_password(pass: &Password) -> bool {
     }
 }
 
-fn parse_lines(input: Vec<String>) -> Vec<Password> {
-    let mut password_list: Vec<Password> = Vec::new();
-    let mut split_strings: Vec<&str>;
-
-    for x in 0..input.len() {
-        split_strings = input[x].split(" ")
-            .collect();
-        if split_strings[0] != "" {
-            password_list.push(
-                Password { 
-                range: split_strings[0]
-                    .split("-")
-                    .collect::<Vec<&str>>()
-                    .iter()
-                    .map(|x| x.parse::<usize>().unwrap())
-                    .collect::<Vec<usize>>(),
-                letter: split_strings[1].trim().chars().collect::<Vec<char>>()[0], 
-                password: split_strings[2].trim().to_string() 
-                });
-        }
-    }
-    password_list
+//Data wrangling
+fn parse_line(input: &String) -> Password {
+    let parsed: Vec<&str> = input.split(['-', ':', ' '].as_ref()).collect();
+    println!("{:?}", parsed);
+    let pass = Password {
+        lower: parsed[0].parse::<usize>().unwrap(),
+        upper: parsed[1].parse::<usize>().unwrap(),
+        letter: parsed[2].chars().collect::<Vec<char>>()[0],
+        // .split() includes the whitespace at index 3, but I can't be 
+        // bothered to figure out a better solution
+        password: parsed[4].to_string()
+    };
+   pass 
 }
+
 
 fn parse_file(file_name: &str) -> Vec<String> {
     let mut file = std::fs::File::open(file_name).unwrap();
