f3dc0c7d800b62ca3ffff03567d50b58283bc7dc
ChrisRG <chris.geekie@gmail.com>
2020-12-06 18:59:31 +0100

    Added part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -9,27 +9,44 @@ fn main() -> Result<(), Box<dyn Error>> {
     let input = std::fs::read_to_string("input.txt").unwrap();
     let groups = input.split("\n\n").collect::<Vec<_>>();
 
-    let final_count = groups
+    let anyone_count = groups
         .iter()
-        .map(|group| calculate_answers(group))
+        .map(|group| answers_from_anyone(group))
         .fold(0, |acc, x| acc + x);
 
-    println!("Total answers for {} groups: {}", groups.len(), final_count);
+    let everyone_count = groups
+        .iter()
+        .map(|group| answers_from_everyone(group))
+        .fold(0, |acc, x| acc + x);
 
+    println!("Total unique answers for {} groups: {}", groups.len(), anyone_count);
+
+    println!("Total unique shared answers: {}", everyone_count);
     Ok(())
 }
 
-fn calculate_answers(input: &str) -> i32 {
-    let lines = input.split_whitespace().collect::<Vec<_>>();
-    let mut answers: HashSet<char> = HashSet::new();
-    let merged: Vec<char>  = lines
+fn answers_from_anyone(input: &str) -> i32 {
+    let group = input.split_whitespace().collect::<Vec<_>>();
+    let total  = group
         .iter()
         .map(|s| s.chars())
-        .flatten().collect();
+        .flatten().collect::<HashSet<_>>()
+        .len();
 
-    for letter in merged {
-        answers.insert(letter);
+    total as i32
+}
+
+fn answers_from_everyone(input: &str) -> i32 {
+    let group = input.split_whitespace().collect::<Vec<_>>();
+    let mut group_sets = group
+        .into_iter()
+        .map(|person| {
+            person.chars().collect::<HashSet<_>>() });
+
+    let mut final_set = group_sets.next().unwrap();
+
+    for set in group_sets {
+        final_set = final_set.intersection(&set).copied().collect();
     }
-
-    answers.len() as i32
+    final_set.len() as i32
 }
