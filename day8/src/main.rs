23e3440d8b3226b1394b9e3173d883cfd8a008a3
ChrisRG <chris.geekie@gmail.com>
2020-12-08 20:15:08 +0100

    Part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -7,37 +7,67 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = std::fs::read_to_string("input.txt").unwrap();
-    let commands: Vec<(&str, i32)> = input
+    let mut commands: Vec<(&str, i32)> = input
         .lines()
         .map(|line| parse_command(line))
         .collect();
-   
-    println!("Accumulator before repeating commands: {}", accumulator(commands));
-    Ok(())
 
+    let run_until_repeat = run(&commands).1;
+
+    println!("Accumulator before repeating commands: {}", run_until_repeat);
+    
+    run_swapping(&mut commands);
+    Ok(())
 }
 
 // Part 1
-fn accumulator(commands: Vec<(&str, i32)>) -> i32{
-    let mut index: i32 = 0; 
-    let mut indices_visited: Vec<bool> = vec![false; commands.len()];
+fn run(commands: &Vec<(&str, i32)>) -> (Vec<usize>, i32) {
+    let mut index: usize = 0; 
+    let mut indices_visited: Vec<usize> = Vec::with_capacity(commands.len());
     let mut accumulator: i32 = 0;
 
-    while !indices_visited[index as usize] {
-        indices_visited[index as usize] = true;
-        match commands[index as usize].0 {
-            "acc" => accumulator += commands[index as usize].1,
-            "jmp" => index += commands[index as usize].1 - 1,
-            "nop" => {},
+    while !indices_visited.contains(&index) {
+        indices_visited.push(index);
+        match commands[index].0 {
+            "acc" => accumulator += commands[index].1,
+            "jmp" => index += commands[index].1 as usize - 1,
+            "nop" => { },
             _ => unreachable!(),
         }
         index += 1;
 
-        if index as usize == commands.len() {
+        if index == commands.len() {
             break;
         }
     }
-    accumulator
+    (indices_visited, accumulator)
+}
+
+// Part 2
+fn swap_jmpnop(command: &mut (&str, i32)) {
+    match command.0 {
+       "jmp" => command.0 = "nop",
+       "nop" => command.0 = "jmp",
+       _ => unreachable!(),
+    }
+}
+
+fn run_swapping(commands: &mut Vec<(&str, i32)>) {
+    for visited in run(&commands).0 {
+
+       if commands[visited].0 == "jmp" || commands[visited].0 == "nop" {
+
+           swap_jmpnop(&mut commands[visited]);
+           let (indices, acc) = run(&commands);
+
+           if indices[indices.len()-1] as usize == commands.len()-1 {
+               println!("Accumulator by end after swapping: {}", acc);
+               break;
+           }
+
+           swap_jmpnop(&mut commands[visited]);
+       }
+    }
 }
 
 // Data Wrangling
